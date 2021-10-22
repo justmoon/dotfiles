0090a2b7c630a887655ca25c2b26301918f802a8
Stefan Thomas <justmoon@members.fsf.org>
2021-10-22 12:15:28 -0700

    feat(arch): install yay and vs code

--- run_0000-bootstrap.sh
+++ run_0000-bootstrap.sh
@@ -7,7 +7,8 @@ CHEZMOI_PATH="$(chezmoi source-path)"
 # Arch install
 if [ -f "/etc/arch-release" ]; then
   which ansible-playbook > /dev/null || sudo pacman -S ansible
-  ansible-playbook -i "localhost," -c local --become-method=sudo $CHEZMOI_PATH/setup/arch.yml
+  ansible-galaxy collection install -r $CHEZMOI_PATH/setup/arch_requirements.yml
+  ansible-playbook -i "localhost," -c local $CHEZMOI_PATH/setup/arch.yml
 else
   echo "Current operating system is not supported in dotfiles bootstrap script"
 fi
