0310cbe632b8fb10c8bf571b57b2ff8e22233c0e
Stefan Thomas <justmoon@members.fsf.org>
2021-10-29 08:31:31 -0700

    feat(arch): update via a simple shell script
    For routine updates, it's very convenient to be able to use an
    interactive shell in case of prompts or messages.

--- setup/update.sh
+++ setup/update.sh
@@ -4,13 +4,22 @@ set -e
 
 CHEZMOI_PATH="$(chezmoi source-path)"
 
-# Make sure sudo password is cached
-sudo true
-
-# Arch install
+# Arch system update
 if [ -f "/etc/arch-release" ]; then
-  which ansible-playbook > /dev/null || { echo "Ansible not installed, did you forget to run 'theia install'?" ; exit 1 ; }
-  ansible-playbook -i "localhost," -c local --become-method=sudo $CHEZMOI_PATH/setup/arch_update.yml
+  echo 'Update packages'
+  yay -Suy
+
+  echo 'Update vim plugins'
+  vim +'PlugUpdate --sync' +qa
+
+  echo 'Update vim-CoC plugins'
+  vim +CocUpdateSync +qa
+
+  echo 'Update VS Code extensions'
+  for ext in `code --list-extensions`
+  do 
+    code --install-extension "$ext" 
+  done
 else
   echo "Current operating system is not supported in dotfiles"
 fi
