b0f135d0b95362a2ce57c025267f507b3bd294a4
Stefan Thomas <justmoon@members.fsf.org>
2022-08-09 09:00:52 -0400

    feat(wez): add hotkeys for switching tabs

--- private_dot_config/wezterm/wezterm.lua
+++ private_dot_config/wezterm/wezterm.lua
@@ -1,6 +1,17 @@
-local wezterm = require 'wezterm'
-local dracula = require 'dracula'
+local wezterm = require("wezterm")
+local dracula = require("dracula")
 local mux = wezterm.mux
+local act = wezterm.action
+
+local mykeys = {}
+for i = 1, 8 do
+	-- CTRL+ALT + number to activate that tab
+	table.insert(mykeys, {
+		key = tostring(i),
+		mods = "CTRL",
+		action = act.ActivateTab(i - 1),
+	})
+end
 
 -- Will become available in next release of Wezterm
 --//wezterm.on("gui-startup", function(cmd)
@@ -9,11 +20,12 @@ local mux = wezterm.mux
 --end)
 
 return {
-  colors = dracula,
-  use_fancy_tab_bar = false,
-  window_decorations = "RESIZE",
-  font = wezterm.font_with_fallback {
-    'MonoLisa',
-    'Symbols Nerd Font',
-  },
+	colors = dracula,
+	use_fancy_tab_bar = false,
+	window_decorations = "RESIZE",
+	font = wezterm.font_with_fallback({
+		"MonoLisa",
+		"Symbols Nerd Font",
+	}),
+	keys = mykeys,
 }
