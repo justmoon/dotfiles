8545f2e37d0aadf41110d728d60007e3752efaca
Stefan Thomas <justmoon@members.fsf.org>
2022-09-09 15:20:24 -0400

    fix(wez): use new url for wezterm color scheme

--- private_dot_config/wezterm/wezterm.lua
+++ private_dot_config/wezterm/wezterm.lua
@@ -1,5 +1,4 @@
 local wezterm = require("wezterm")
-local dracula = require("dracula")
 local mux = wezterm.mux
 local act = wezterm.action
 
@@ -13,14 +12,14 @@ for i = 1, 8 do
 	})
 end
 
--- Will become available in next release of Wezterm
---//wezterm.on("gui-startup", function(cmd)
---  local tab, pane, window = mux.spawn_window(cmd or {})
---  window:gui_window():maximize()
---end)
+-- Start maximized
+wezterm.on("gui-startup", function(cmd)
+	local tab, pane, window = mux.spawn_window(cmd or {})
+	window:gui_window():maximize()
+end)
 
 return {
-	colors = dracula,
+	color_scheme = "Dracula (Official)",
 	use_fancy_tab_bar = false,
 	window_decorations = "RESIZE",
 	font = wezterm.font_with_fallback({
