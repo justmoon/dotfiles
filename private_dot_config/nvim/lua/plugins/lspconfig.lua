8dd4a9a18dbb5f87974d83b6750349137f1560d1
Stefan Thomas <justmoon@members.fsf.org>
2023-04-28 09:22:31 -0400

    feat(nvim): better workspace root setting for eslint,tsserver

--- private_dot_config/nvim/lua/plugins/lspconfig.lua
+++ private_dot_config/nvim/lua/plugins/lspconfig.lua
@@ -1,3 +1,5 @@
+local util = require("lspconfig").util
+
 return {
   {
     "neovim/nvim-lspconfig",
@@ -8,8 +10,29 @@ return {
         -- TOML
         taplo = {
           root_dir = function(fname)
-            local lspconfig = require("lspconfig")
-            return lspconfig.util.root_pattern("taplo.toml", ".git")(fname) or vim.loop.os_homedir()
+            return util.root_pattern("taplo.toml", ".git")(fname) or vim.loop.os_homedir()
+          end,
+        },
+        -- ESLint
+        eslint = {
+          root_dir = function(fname)
+            return util.root_pattern("pnpm-workspace.yaml")(fname)
+              or util.root_pattern("turbo.json")(fname)
+              or util.root_pattern(".git")(fname)
+              or util.root_pattern("package.json")(fname)
+              or util.root_pattern(".eslintrc.js")(fname)
+          end,
+        },
+        -- TypeScript
+        tsserver = {
+          root_dir = function(fname)
+            return util.root_pattern("pnpm-workspace.yaml")(fname)
+              or util.root_pattern("turbo.json")(fname)
+              or util.root_pattern("tsconfig.base.json")(fname)
+              or util.root_pattern(".git")(fname)
+              or util.root_pattern("package.json")(fname)
+              or util.root_pattern("tsconfig.json")(fname)
+              or util.root_pattern("jsconfig.json")(fname)
           end,
         },
       },
