595984936f4bc038504e2217c91b6d25d8749547
Stefan Thomas <justmoon@members.fsf.org>
2023-05-08 13:46:06 -0400

    refactor(wez): update wezterm config to use best practices

--- private_dot_config/wezterm/wezterm.lua
+++ private_dot_config/wezterm/wezterm.lua
@@ -2,10 +2,20 @@ local wezterm = require("wezterm")
 local mux = wezterm.mux
 local act = wezterm.action
 
-local mykeys = {}
+-- This table will hold the configuration.
+local config = {}
+
+-- In newer versions of wezterm, use the config_builder which will
+-- help provide clearer error messages
+if wezterm.config_builder then
+	config = wezterm.config_builder()
+end
+
+config.keys = {}
+
 for i = 1, 8 do
-	-- CTRL+ALT + number to activate that tab
-	table.insert(mykeys, {
+	-- CTRL + number to activate that tab
+	table.insert(config.keys, {
 		key = tostring(i),
 		mods = "CTRL",
 		action = act.ActivateTab(i - 1),
@@ -14,23 +24,30 @@ end
 
 -- Start maximized
 wezterm.on("gui-startup", function(cmd)
-	local tab, pane, window = mux.spawn_window(cmd or {})
+	local _, _, window = mux.spawn_window(cmd or {})
 	window:gui_window():maximize()
 end)
 
-return {
-	color_scheme = "Dracula (Official)",
-	use_fancy_tab_bar = false,
-	hide_tab_bar_if_only_one_tab = true,
-	window_decorations = "RESIZE",
-	font = wezterm.font_with_fallback({
-		"MonoLisa",
-		"IBM Plex Mono",
-		"Symbols Nerd Font",
-	}),
-	keys = mykeys,
-	check_for_updates = false,
-	enable_scroll_bar = true,
-	initial_cols = 100,
-	initial_rows = 28,
-}
+config.color_scheme = "Dracula (Official)"
+
+config.use_fancy_tab_bar = false
+
+config.hide_tab_bar_if_only_one_tab = true
+
+config.window_decorations = "RESIZE"
+
+config.font = wezterm.font_with_fallback({
+	"MonoLisa",
+	"IBM Plex Mono",
+	"Symbols Nerd Font",
+})
+
+config.check_for_updates = false
+
+config.enable_scroll_bar = true
+
+config.initial_cols = 100
+
+config.initial_rows = 28
+
+return config
